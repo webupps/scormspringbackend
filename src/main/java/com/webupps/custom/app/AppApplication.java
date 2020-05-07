3cccc470c6281af2d8f378f623f9a12e4d98e68f
johnpaulmariano <jmariano@webupps.com>
2020-05-06 21:32:56 -0400

    Setup basic libraries

--- src/main/java/com/webupps/custom/app/AppApplication.java
+++ src/main/java/com/webupps/custom/app/AppApplication.java
@@ -1,9 +1,17 @@
 package com.webupps.custom.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.webupps.custom")
+@EntityScan("com.webupps.custom")
+@EnableJpaRepositories("com.webupps.custom")
 public class AppApplication {
 
 	public static void main(String[] args) {
